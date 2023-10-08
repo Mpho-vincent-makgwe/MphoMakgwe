7026ea40c57f73bb409a304384baba1a7a227412
Mpho Vincent Makgwe <mphomakgwe$@gmail.com>
2023-10-08 17:58:55 +0200

    optomisng for smaller screens

--- components/RepoStructures/commonRepo.tsx
+++ components/RepoStructures/commonRepo.tsx
@@ -7,9 +7,9 @@ const CommonRepo = ({ repo }: any) => {
   return (
     <div>
       <div className={`bg-gray-100 p-4 m-2 rounded-lg shadow-md`}>
-          <p className={`font-bold text-2xl`}>{repo.name}</p>
+          <p className={`font-bold text-2xl overflow-hidden whitespace-nowrap overflow-ellipsis`}>{repo.name}</p>
           <p className={`text-gray-700`}>{repo.description}</p>
-          
+
           <div className={`flex space-x-2 items-center text-gray-600`}>
             <span>
               <FaStar /> {repo.stargazers_count}
